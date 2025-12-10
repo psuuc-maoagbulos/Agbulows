6782ef0be202063e9d9146cfb460f888c97fbf07
psuuc-maoagbulos <93295788+psuuc-maoagbulos@users.noreply.github.com>
2025-12-11 06:55:28 +0800

    Delete mongoose/models/user copy.js

--- mongoose/models/user copy.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  email: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('User', UserSchema);
